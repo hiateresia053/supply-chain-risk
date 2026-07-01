d008c263a73a4683676bdedf88e7d1a88725c78c
Teresia Hia <hiateresia053@gmail.com>
2026-07-01 23:06:18 +0700

    Create database migrations

---
+++ database/migrations/2026_07_01_151424_create_countries_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('countries', function (Blueprint $table) {
+    $table->id();
+
+    $table->string('country_name');
+    $table->string('country_code', 10)->unique();
+
+    $table->string('currency')->nullable();
+
+    $table->string('region')->nullable();
+
+    $table->bigInteger('population')->nullable();
+
+    $table->decimal('gdp', 20, 2)->nullable();
+
+    $table->decimal('inflation_rate', 8, 2)->nullable();
+
+    $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('countries');
+    }
+};
---
+++ database/migrations/2026_07_01_151451_create_ports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ports', function (Blueprint $table) {
+    $table->id();
+
+    $table->foreignId('country_id')
+          ->constrained('countries')
+          ->onDelete('cascade');
+
+    $table->string('port_name');
+
+    $table->decimal('latitude', 10, 7);
+
+    $table->decimal('longitude', 10, 7);
+
+    $table->string('port_code')->nullable();
+
+    $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ports');
+    }
+};
